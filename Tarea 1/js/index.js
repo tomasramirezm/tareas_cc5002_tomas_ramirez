2e4ab360b65276249b19d0c34b85bd8dd1da37c2
Tomás Ramírez <samot.0309@gmail.com>
2025-08-29 06:05:23 +0200

    Se crea la portada y todas sus funcionalidades

---
+++ Tarea 1/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-agregar").addEventListener("click", function() {
+    window.location.href = "agregar_aviso.html";
+});
+
+document.getElementById("btn-ver").addEventListener("click", function() {
+    window.location.href = "pagina-listado.html";
+});
+
+document.getElementById("btn-estadisticas").addEventListener("click", function() {
+    window.location.href = "pagina-estadisticas.html"; 
+});
