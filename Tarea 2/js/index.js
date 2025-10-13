cdd0ec48692e344311821d2b1b466d38a527795f
Tomás Ramírez <samot.0309@gmail.com>
2025-10-13 19:53:04 +0200

    Copiamos todo lo de la Tarea 1 a la carpeta para la Tarea 2

---
+++ Tarea 2/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-agregar").addEventListener("click", function() {
+    window.location.href = "agregar_aviso.html";
+});
+
+document.getElementById("btn-ver").addEventListener("click", function() {
+    window.location.href = "listado.html";
+});
+
+document.getElementById("btn-estadisticas").addEventListener("click", function() {
+    window.location.href = "estadisticas.html"; 
+});
